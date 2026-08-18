dd7a327eb75c027df8fd99f88301ea16715445de
Taniya-eGov <taniya@egovernments.org>
2026-08-18 10:29:17 +0530

    added product_variant table schema for bronze layer

--- ClickHouse_ddl/01_kafka_event_consumers.sql
+++ ClickHouse_ddl/01_kafka_event_consumers.sql
@@ -232,6 +232,20 @@ SETTINGS
     kafka_max_block_size = 65536,
     kafka_skip_broken_messages = 100;
 
+CREATE TABLE IF NOT EXISTS analytics.kafka_product_variant_events
+(
+    raw String
+)
+ENGINE = Kafka
+SETTINGS
+    kafka_broker_list = 'release-name-kafka.kafka-kraft.svc.cluster.local:9092',
+    kafka_topic_list = 'clickhouse-product-variant-events',
+    kafka_group_name = 'clickhouse-product-variant-consumer',
+    kafka_format = 'JSONAsString',
+    kafka_num_consumers = 1,
+    kafka_max_block_size = 65536,
+    kafka_skip_broken_messages = 100;
+
 CREATE TABLE IF NOT EXISTS analytics.kafka_service_events
 (
     raw String
